98c2d80ab97afe13de50bb566af5ba6da50e3870
Ahmed Tawfik <abtawfik@umich.edu>
2019-04-20 14:20:37 -0400

    metric revamp:  started working on translating the ctp high low to cleaner python code and created the constant.py file that will hold all the constants

---
+++ metrics/constants.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+Created Apr 20 2019
+
+Contains constants used across metrics
+
+@tawfik
+'''
+
+# Gas constant for dry air [J kg-1 K-1]
+Rd = 287.04
+
+# Specific heat capacity of dry air [J kg-1]
+cp=1005.7
+
+# Ratio of Gas constant and specific heat capacity [K-1]
+R_cp=Rd/cp
+
+# Conversion from Celcius to Kelvin
+C2K = 273.15
+
+# Latent heat of condensation [J kg-1]
+Lv=2.5e6
+
+# Ratio of latent heat of cond. and specific heat capacity [unitless]
+Lv_cp=Lv/cp
+
+# Gas constant for moist air [J kg-1 K-1]
+Rv=461.5
+
+# Acceleration due to gravity [m s-2]
+grav= 9.81
+
+# ----
+ep=0.622
